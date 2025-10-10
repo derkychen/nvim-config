782a7b18cb38c94ae1f8caaf76ceeac5bc9a8afc
Derek Chen <derekjchen@proton.me>
2025-10-10 10:11:20 -0400

    new keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,16 +1,26 @@
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, {})
+
 -- Toggle diagnostics
 vim.keymap.set("n", "<leader>dt", function()
-  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+	vim.diagnostic.enable(not vim.diagnostic.is_enabled())
 end, {})
 
 -- Navigate between windows (including terminal) with hjkl
-vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], {noremap = true})
-vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], {noremap = true})
-vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], {noremap = true})
-vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], {noremap = true})
+vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], { noremap = true })
+vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], { noremap = true })
+vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], { noremap = true })
+vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], { noremap = true })
 
-vim.keymap.set("n", "<C-h>", [[<C-w>h]], {noremap = true})
-vim.keymap.set("n", "<C-j>", [[<C-w>j]], {noremap = true})
-vim.keymap.set("n", "<C-k>", [[<C-w>k]], {noremap = true})
-vim.keymap.set("n", "<C-l>", [[<C-w>l]], {noremap = true})
-vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {}) 
+vim.keymap.set("n", "<C-h>", [[<C-w>h]], { noremap = true })
+vim.keymap.set("n", "<C-j>", [[<C-w>j]], { noremap = true })
+vim.keymap.set("n", "<C-k>", [[<C-w>k]], { noremap = true })
+vim.keymap.set("n", "<C-l>", [[<C-w>l]], { noremap = true })
+
+vim.keymap.set("i", "<C-h>", [[<Esc><C-w>h]], { noremap = true })
+vim.keymap.set("i", "<C-j>", [[<Esc><C-w>j]], { noremap = true })
+vim.keymap.set("i", "<C-k>", [[<Esc><C-w>k]], { noremap = true })
+vim.keymap.set("i", "<C-l>", [[<Esc><C-w>l]], { noremap = true })
+
+vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,12 @@ return {
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<Leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<Leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<Leader>fr", builtin.oldfiles, {})
+    end
 	},
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
@@ -21,6 +27,7 @@ return {
     dir = "~/Developer/dirjump/",
     config = function()
       require("telescope").load_extension("dirjump")
+      vim.keymap.set("n", "<Leader>fd", ":Telescope dirjump", {})
     end
   }
 }
