e6d3f1a1b4c053372dbb5977fef39cdb4f956737
Derek Chen <derekjchen@proton.me>
2025-10-13 14:40:11 -0400

    using default layout capabilities with alpha nvim, fixed oldfiles typo

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,68 +1,54 @@
 return {
   "goolord/alpha-nvim",
   config = function()
-    local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
+
+    -- Header
+    dashboard.section.header.val = {
+      "  ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆         ",
+      "   ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦      ",
+      "         ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄    ",
+      "          ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀ ⠢⣀⡀⠈⠙⠿⠄   ",
+      "         ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀  ",
+      "  ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄ ",
+      " ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄  ",
+      "⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄ ",
+      "⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄",
+      "     ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆    ",
+      "      ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⣿⠃    ",
+      "        ⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠁      ",
+    }
+
+    -- NVIM version
+    dashboard.section.nvim = {
+      type = "text",
+      val = ("NVIM v%d.%d.%d%s"):format(vim.version().major, vim.version().minor, vim.version().patch,
+        vim.version().prerelease and "-dev" or ""),
+      opts = { position = "center" },
+    }
 
     -- Buttons
     dashboard.section.buttons.val = {
       dashboard.button("n", "+  " .. "> New file", "<cmd> ene <BAR> startinsert <cr>"),
-      dashboard.button("r", "󰽙  " .. "> Recent files", "<cmd> Telescope old_files <cr>"),
+      dashboard.button("r", "󰽙  " .. "> Recent files", "<cmd> Telescope oldfiles <cr>"),
       dashboard.button("f", "  " .. "> Find file", "<cmd> Telescope find_files <cr>"),
       dashboard.button("d", "  " .. "> Change directory", "<cmd> Telescope dirjump <cr>"),
       dashboard.button("c", "  " .. "> Config", "<cmd> e ~/.config/nvim/ <cr>"),
       dashboard.button("q", "  " .. "> Quit", "<cmd> q <cr>"),
     }
-    dashboard.opts.layout[1].val = 20
+
+    dashboard.config.layout = {
+      { type = "padding", val = 20 },
+      dashboard.section.header,
+      { type = "padding", val = 1 },
+      dashboard.section.nvim,
+      { type = "padding", val = 2 },
+      dashboard.section.buttons,
+    }
 
     -- Setup
-    alpha.setup(dashboard.opts)
+    require("alpha").setup(dashboard.config)
 
-    -- Header and footer
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "AlphaReady",
-      once = true,
-      callback = function()
-        local hydra = {
-          "  ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆         ",
-          "   ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦      ",
-          "         ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄    ",
-          "          ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀ ⠢⣀⡀⠈⠙⠿⠄   ",
-          "         ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀  ",
-          "  ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄ ",
-          " ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄  ",
-          "⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄ ",
-          "⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄",
-          "     ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆    ",
-          "      ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⣿⠃    ",
-          "        ⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠛⠉       ",
-        }
-        local banner = hydra
-
-        local v = vim.version()
-        local nvim = ("NVIM v%d.%d.%d%s"):format(v.major, v.minor, v.patch, v.prerelease and "-dev" or "")
-
-        -- Centering
-        local b = vim.fn.strdisplaywidth(banner[1])
-        local n = vim.fn.strdisplaywidth(nvim)
-        local left = math.floor(math.abs((b - n) / 2));
-        if b > n then
-          nvim = string.rep(" ", left) .. nvim
-        elseif n > b then
-          for i, s in ipairs(banner) do
-            banner[i] = string.rep(" ", left) .. s
-          end
-        end
-
-        local header = {}
-        table.move(banner, 1, #banner, #header + 1, header)
-        table.insert(header, "")
-        table.insert(header, nvim)
-
-        dashboard.section.header.val = header
-        pcall(vim.cmd.AlphaRedraw)
-      end,
-    })
     -- Auto-open Alpha when creating a new tab (only if it's an empty scratch buffer)
     vim.api.nvim_create_autocmd("TabNew", {
       callback = function()
