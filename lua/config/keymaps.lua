1963d3898533fbfbc016748e19b98a325ed226e3
Derek Chen <derekjchen@proton.me>
2025-11-14 06:54:18 -0500

    deleted window jump keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,19 +1,3 @@
--- Navigate between windows in normal, terminal, and insert mode with hjkl
-vim.keymap.set("n", "<C-h>", [[<C-w>h]], {})
-vim.keymap.set("n", "<C-j>", [[<C-w>j]], {})
-vim.keymap.set("n", "<C-k>", [[<C-w>k]], {})
-vim.keymap.set("n", "<C-l>", [[<C-w>l]], {})
-
-vim.keymap.set("t", "<C-h>", [[<C-\><C-n><C-w>h]], {})
-vim.keymap.set("t", "<C-j>", [[<C-\><C-n><C-w>j]], {})
-vim.keymap.set("t", "<C-k>", [[<C-\><C-n><C-w>k]], {})
-vim.keymap.set("t", "<C-l>", [[<C-\><C-n><C-w>l]], {})
-
-vim.keymap.set("i", "<C-h>", [[<Esc><C-w>h]], {})
-vim.keymap.set("i", "<C-j>", [[<Esc><C-w>j]], {})
-vim.keymap.set("i", "<C-k>", [[<Esc><C-w>k]], {})
-vim.keymap.set("i", "<C-l>", [[<Esc><C-w>l]], {})
-
 -- Tabs
 vim.keymap.set("n", "<Leader>tn", function()
   vim.cmd("tabnew")
