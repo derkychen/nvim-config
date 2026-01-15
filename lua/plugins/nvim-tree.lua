6f3994b3976dda74d5ba1b4af3d50e860b53dfe4
Derek Chen <derekjchen@proton.me>
2026-01-15 10:27:27 -0500

    new plugins

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,39 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-mini/mini.icons",
+  },
+  config = function()
+    require("nvim-tree").setup({
+      view = {
+        width = 35,
+      },
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          git_placement = "right_align",
+          diagnostics_placement = "right_align",
+          modified_placement = "right_align",
+          hidden_placement = "right_align",
+          bookmarks_placement = "right_align",
+          glyphs = {
+            git = {
+              untracked = ""
+            },
+          },
+        },
+      },
+      update_focused_file = {
+        enable = true,
+      },
+    })
+
+    vim.keymap.set("n", "<Leader>e", function()
+      vim.cmd("NvimTreeToggle")
+    end, {desc="Toggle nvim-tree"})
+  end,
+}
