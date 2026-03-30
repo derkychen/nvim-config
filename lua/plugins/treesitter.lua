5b7357019bba0520507762f920165ae95766af2a
Derek Chen <derekjchen@proton.me>
2026-03-29 21:13:05 -0400

    manually manage parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,42 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
   build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      highlight = { enable = true },
-      indent = { enable = true },
-      auto_install = true,
+    require("nvim-treesitter").setup()
+    local parsers = {
+      "lua",
+      "latex",
+      "r",
+      "c",
+      "python",
+      "markdown",
+      "markdown_inline",
+      "html",
+      "css",
+      "javascript",
+      "bash",
+      "toml",
+    }
+    local filetypes = {
+      "lua",
+      "tex",
+      "r",
+      "c",
+      "python",
+      "markdown",
+      "html",
+      "css",
+      "javascript",
+      "sh",
+      "toml",
+    }
+    require("nvim-treesitter").install(parsers)
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = filetypes,
+      callback = function(ev)
+        pcall(vim.treesitter.start, ev.buf)
+      end,
     })
-  end,
+  end
 }
