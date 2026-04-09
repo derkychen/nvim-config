6e0ae5b8b614617d21c42991e36c554def5a9757
Derek Chen <derekjchen@proton.me>
2026-04-08 21:35:57 -0400

    change winseparator color

--- plugin/10_tokyonight.lua
+++ plugin/10_tokyonight.lua
@@ -3,7 +3,7 @@ vim.pack.add({ "https://github.com/folke/tokyonight.nvim" })
 require("tokyonight").setup({
   on_highlights = function(hl, c)
     hl.TabLineFill = { bg = c.bg_dark1 }
-    hl.WinSeparator = { fg = c.bg_highlight }
+    hl.WinSeparator = { fg = c.fg_gutter }
     hl.WinBar = { bg = "none" }
     hl.WinBarNC = { fg = c.fg_gutter, bg = "none" }
   end,
