858f6218da6e8633ed8fd0af24865dd6bbb1e14d
Derek Chen <derekjchen@proton.me>
2026-04-12 21:11:39 -0400

    minor formatting refactors

--- lua/utils.lua
+++ lua/utils.lua
@@ -5,16 +5,16 @@ function M.valid_normal_buf(buf)
   local bufname = vim.api.nvim_buf_get_name(buf)
   local buftype = vim.api.nvim_get_option_value("buftype", { buf = buf })
 
-  -- I think this is a bug with Oil specifically:
+  -- TODO: Remove once this Oil bug is fixed:
   -- https://github.com/stevearc/oil.nvim/issues/710
   local filetype = vim.api.nvim_get_option_value("filetype", { buf = buf })
   local other_conds = filetype ~= "oil" and not bufname:match("^oil://")
 
   return vim.api.nvim_buf_is_valid(buf)
-      and buftype == ""
-      and bufname ~= nil
-      and bufname ~= ""
-      and other_conds
+    and buftype == ""
+    and bufname ~= nil
+    and bufname ~= ""
+    and other_conds
 end
 
 return M
