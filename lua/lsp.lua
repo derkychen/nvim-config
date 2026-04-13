2adfb54e2d5fde4755bb69672d6d3cf6359bbad5
Derek Chen <derekjchen@proton.me>
2026-04-12 21:23:27 -0400

    deleted stylua

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -7,16 +7,15 @@ vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
       })
     end
     vim.lsp.enable({
-      "lua_ls", -- Lua LSP
-      "stylua", -- Lua formatter
-      "ruff", -- Python LSP, linter, and formatter
-      "texlab", -- LaTeX LSP, latexindent for formatting
-      "clangd", -- C LSP and formatter
+      "lua_ls",         -- Lua LSP and formatter
+      "ruff",           -- Python LSP, linter, and formatter
+      "texlab",         -- LaTeX LSP, latexindent for formatting
+      "clangd",         -- C LSP and formatter
       "markdown_oxide", -- Markdown LSP-ish, have yet to explore PKM
-      "remark_ls", -- Markdown formatter
-      "biome", -- Web dev LSP and formatter
-      "bashls", -- Shell script LSP, shfmt for formatting
-      "tombi", -- TOML LSP and formatter
+      "remark_ls",      -- Markdown formatter
+      "biome",          -- Web dev LSP and formatter
+      "bashls",         -- Shell script LSP, shfmt for formatting
+      "tombi",          -- TOML LSP and formatter
     })
   end,
   once = true,
