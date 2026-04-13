cd9a2152db75342e453e855e1ffa1600cfb3ccd8
Derek Chen <derekjchen@proton.me>
2026-04-12 21:04:41 -0400

    feat: made lsp loading lazier, added blink integration

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,25 +1,23 @@
--- Lua
-vim.lsp.enable("lua_ls")
-
--- Python
-vim.lsp.enable("ruff")
-
--- LaTeX
-vim.lsp.enable("texlab")
-vim.lsp.enable("latexindent")
-
--- C
-vim.lsp.enable("clangd")
-
--- Markdown
-vim.lsp.enable("markdown_oxide")
-vim.lsp.enable("remark_ls")
-
--- Web dev
-vim.lsp.enable("biome")
-
--- Shell scripting
-vim.lsp.enable("bashls")
-
--- TOML
-vim.lsp.enable("tombi")
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+  callback = function()
+    local ok, blink = pcall(require, "blink.cmp")
+    if ok then
+      vim.lsp.config("*", {
+        capabilities = blink.get_lsp_capabilities(nil, true),
+      })
+    end
+    vim.lsp.enable({
+      "lua_ls", -- Lua LSP
+      "stylua", -- Lua formatter
+      "ruff", -- Python LSP, linter, and formatter
+      "texlab", -- LaTeX LSP, latexindent for formatting
+      "clangd", -- C LSP and formatter
+      "markdown_oxide", -- Markdown LSP-ish, have yet to explore PKM
+      "remark_ls", -- Markdown formatter
+      "biome", -- Web dev LSP and formatter
+      "bashls", -- Shell script LSP, shfmt for formatting
+      "tombi", -- TOML LSP and formatter
+    })
+  end,
+  once = true,
+})
