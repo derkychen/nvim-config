1e8f1edc4dcbb154d57826cff4bd691e2eaf2567
Derek Chen <derekjchen@proton.me>
2026-04-12 21:11:01 -0400

    fix: actually setup autopairs and add rules for latex

--- plugin/autopairs.lua
+++ plugin/autopairs.lua
@@ -1,1 +1,11 @@
 vim.pack.add({ "https://github.com/windwp/nvim-autopairs" })
+
+local Rule = require("nvim-autopairs.rule")
+local npairs = require("nvim-autopairs")
+
+npairs.setup()
+
+npairs.add_rules({
+  Rule("\\(", "\\)", "tex"),
+  Rule("\\[", "\\]", "tex")
+})
