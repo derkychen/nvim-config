04f002ee274954b595a0c2d0f86dcf49d21dd658
Derek Chen <derekjchen@proton.me>
2026-04-24 12:11:00 -0400

    feat: add acknowledgement to the goat

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,11 +1,16 @@
+-- Setup all LSP servers
+-- Based on and thanks to MariaSolOs's dotfiles:
+-- https://github.com/MariaSolOs/dotfiles
 vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
   callback = function()
+    -- Extend Neovim's client capabilities with the completion ones
     local ok, blink = pcall(require, "blink.cmp")
     if ok then
       vim.lsp.config("*", {
         capabilities = blink.get_lsp_capabilities(nil, true),
       })
     end
+
     vim.lsp.enable({
       "lua_ls",         -- Lua LSP and formatter
       "ruff",           -- Python LSP, linter, and formatter
