273deaf685a5fbb353eab672e728db0e567668c1
Derek Chen <derekjchen@proton.me>
2026-04-30 17:41:49 -0400

    fix: format any markdown file instead of those in a workspace

--- lsp/remark_ls.lua
+++ lsp/remark_ls.lua
@@ -1,15 +1,27 @@
 return {
-  settings = { remark = { requireConfig = false } },
   cmd = { "remark-language-server", "--stdio" },
   filetypes = { "markdown" },
-  root_markers = {
-    ".remarkrc",
-    ".remarkrc.json",
-    ".remarkrc.js",
-    ".remarkrc.cjs",
-    ".remarkrc.mjs",
-    ".remarkrc.yml",
-    ".remarkrc.yaml",
-    ".remarkignore",
+  settings = {
+    remark = {
+      requireConfig = false,
+    },
   },
+  root_dir = function(bufnr, on_dir)
+    local name = vim.api.nvim_buf_get_name(bufnr)
+    local path = name ~= "" and vim.fs.dirname(name) or vim.uv.cwd()
+    local root = vim.fs.find({
+      ".remarkrc",
+      ".remarkrc.json",
+      ".remarkrc.js",
+      ".remarkrc.cjs",
+      ".remarkrc.mjs",
+      ".remarkrc.yml",
+      ".remarkrc.yaml",
+      ".remarkignore",
+    }, {
+      path = path,
+      upward = true,
+    })[1]
+    on_dir(root and vim.fs.dirname(root) or path)
+  end,
 }
