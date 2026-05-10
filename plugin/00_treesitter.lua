8f1301c3e5c442736fdeeaa7d3cc4d3b7cee8db4
Derek Chen <derekjchen@proton.me>
2026-05-10 19:41:00 +0200

    minor changes

--- plugin/00_treesitter.lua
+++ plugin/00_treesitter.lua
@@ -1,5 +1,8 @@
--- TODO: Migrate to tree-sitter-manager.nvim if it becomes stable enough, and if
--- nvim-treesitter stays archived
+-- TODO: Migrate to native Tree-sitter support when it becomes available:
+-- https://github.com/neovim/neovim/issues/39006
+
+-- Based on and thanks Evgeni Chasnovski's Neovim configuration:
+-- https://github.com/echasnovski/nvim
 vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
 
 local languages = {
@@ -10,6 +13,7 @@ local languages = {
   "csv",
   "html",
   "javascript",
+  "json",
   "latex",
   "lua",
   "markdown",
@@ -18,8 +22,9 @@ local languages = {
 }
 
 -- Ensure parsers for above languages are installed
-local isnt_installed = function(lang)
-  return #vim.api.nvim_get_runtime_file("parser/" .. lang .. ".*", false) == 0
+local isnt_installed = function(language)
+  return
+      #vim.api.nvim_get_runtime_file("parser/" .. language .. ".*", false) == 0
 end
 local to_install = vim.tbl_filter(isnt_installed, languages)
 if #to_install > 0 then
@@ -28,13 +33,13 @@ end
 
 -- Start Tree-sitter on buffers of filetypes corresponding to languages
 local treesitter_start_group =
-  vim.api.nvim_create_augroup("TreesitterStart", { clear = true })
+    vim.api.nvim_create_augroup("TreesitterStart", { clear = true })
 
 local filetypes = vim
-  .iter(languages)
-  :map(vim.treesitter.language.get_filetypes)
-  :flatten()
-  :totable()
+    .iter(languages)
+    :map(vim.treesitter.language.get_filetypes)
+    :flatten()
+    :totable()
 
 vim.api.nvim_create_autocmd("FileType", {
   callback = function(ev)
