c7954f23b09983000ed31a19d57df39118c24a19
Derek Chen <derekjchen@proton.me>
2026-08-10 19:19:28 +0200

    feat: comment + doxygen parsing

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,32 +1,75 @@
+-- LSP functionality.
+--
+-- Adjusts semantic highlighting to accommodate for Tree-sitter highlighting and
+-- sets up LSP servers lazily.
 local M = {}
 
+-- Clear semantic highlights for comments in Lua.
+local function set_semantic_hls()
+  vim.api.nvim_set_hl(0, "@lsp.type.comment.lua", {})
+end
+
 function M.setup()
+  -- Set semantic highlights.
+  set_semantic_hls()
+
+  local semantic_hls_group =
+    vim.api.nvim_create_augroup("LSPSemanticHighlights", { clear = true })
+
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = set_semantic_hls,
+    group = semantic_hls_group,
+  })
+
   -- Lazily setup all LSP servers.
   --
   -- Based on and thanks to Maria Solano's dotfiles:
   -- https://github.com/MariaSolOs/dotfiles
+  local lsp_setup_group =
+    vim.api.nvim_create_augroup("LSPSetup", { clear = true })
+
   vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
     callback = function()
-      local servers = vim.iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
-          :map(function(file)
-            return vim.fn.fnamemodify(file, ":t:r")
-          end)
-          :totable()
+      local servers = vim
+        -- NOTE: THe file name of the LSP configuration can differ from the name
+        --       of the server.
+        .iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
+        :map(function(path)
+          return vim.fs.basename(path):match("^(.*)%.lua$")
+        end)
+        :totable()
 
       vim.lsp.enable(servers)
     end,
+    group = lsp_setup_group,
     once = true,
   })
 
   -- LSP
-  vim.keymap.set("n", "<Leader>lh", vim.lsp.buf.hover,
-    { desc = "LSP buffer hover" })
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition,
-    { desc = "LSP goto definition" })
-  vim.keymap.set("n", "<Leader>la", vim.lsp.buf.code_action,
-    { desc = "LSP code action" })
-  vim.keymap.set("n", "<Leader>lf", vim.lsp.buf.format,
-    { desc = "LSP format buffer" })
+  vim.keymap.set(
+    "n",
+    "<Leader>lh",
+    vim.lsp.buf.hover,
+    { desc = "LSP buffer hover" }
+  )
+  vim.keymap.set(
+    "n",
+    "gd",
+    vim.lsp.buf.definition,
+    { desc = "LSP goto definition" }
+  )
+  vim.keymap.set(
+    "n",
+    "<Leader>la",
+    vim.lsp.buf.code_action,
+    { desc = "LSP code action" }
+  )
+  vim.keymap.set(
+    "n",
+    "<Leader>lf",
+    vim.lsp.buf.format,
+    { desc = "LSP format buffer" }
+  )
 end
 
 return M
--- plugin/00_treesitter.lua
+++ plugin/00_treesitter.lua
@@ -1,3 +1,8 @@
+-- Configuration for the `nvim-treesitter` plugin.
+--
+-- Ensures parsers for a set of languages are installed. Automatically starts
+-- Tree-sitter on buffers with filetypes matching these languages.
+--
 -- TODO: Migrate to native Tree-sitter support when it becomes available:
 --       https://github.com/neovim/neovim/issues/39006
 vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
@@ -7,8 +12,11 @@ local languages = {
   "bibtex",
   "c",
   "cmake",
+  "comment",
+  "cpp",
   "css",
   "csv",
+  "doxygen",
   "html",
   "javascript",
   "json",
@@ -21,19 +29,25 @@ local languages = {
   "yaml",
 }
 
+require("nvim-treesitter").install(languages)
+
+-- Start Tree-sitter on relevant buffers.
+--
 -- Based on and thanks Evgeni Chasnovski's Neovim configuration:
 -- https://github.com/echasnovski/nvim
-local isnt_installed = function(lang) return #vim.api.nvim_get_runtime_file(
-  "parser/" .. lang .. ".*", false) == 0 end
-local to_install = vim.tbl_filter(isnt_installed, languages)
-if #to_install > 0 then require("nvim-treesitter").install(to_install) end
+local filetypes = vim
+  .iter(languages)
+  :map(vim.treesitter.language.get_filetypes)
+  :flatten()
+  :totable()
 
-local filetypes = vim.iter(languages):map(vim.treesitter.language
-.get_filetypes):flatten():totable()
+local treesitter_start_group =
+  vim.api.nvim_create_augroup("TreesitterStart", { clear = true })
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = filetypes,
   callback = function(ev)
     pcall(vim.treesitter.start, ev.buf)
   end,
+  group = treesitter_start_group,
 })
