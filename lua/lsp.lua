b0338d662384da514f4dabcbab5fc581d8d2e3cf
Derek Chen <derekjchen@proton.me>
2026-08-26 09:34:34 +0200

    fix: remove lazy lsp behaviour as it causes restart bugs on 0.12.5, enable is lazy enough.

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,75 +1,45 @@
--- LSP functionality.
---
--- Adjusts semantic highlighting to accommodate for Tree-sitter highlighting and
--- sets up LSP servers lazily.
+---LSP functionality.
+---
+---Adjusts semantic highlighting to accommodate for Tree-sitter highlighting and
+---sets up LSP servers lazily.
 local M = {}
 
--- Clear semantic highlights for comments in Lua.
+---Clear semantic highlights for comments in Lua.
 local function set_semantic_hls()
   vim.api.nvim_set_hl(0, "@lsp.type.comment.lua", {})
 end
 
+---Set up LSP functionality.
 function M.setup()
+  -- Set up all LSP servers.
+  --
+  -- NOTE: THe file name of the LSP configuration can differ from the name
+  --       of the server.
+  local servers = vim
+    .iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
+    :map(function(path)
+      return vim.fs.basename(path):match("^(.*)%.lua$")
+    end)
+    :totable()
+
+  vim.lsp.enable(servers)
+
   -- Set semantic highlights.
   set_semantic_hls()
 
   local semantic_hls_group =
     vim.api.nvim_create_augroup("LSPSemanticHighlights", { clear = true })
 
+  -- Update semantic highlights on a colour scheme change.
   vim.api.nvim_create_autocmd("ColorScheme", {
     callback = set_semantic_hls,
     group = semantic_hls_group,
   })
 
-  -- Lazily setup all LSP servers.
-  --
-  -- Based on and thanks to Maria Solano's dotfiles:
-  -- https://github.com/MariaSolOs/dotfiles
-  local lsp_setup_group =
-    vim.api.nvim_create_augroup("LSPSetup", { clear = true })
-
-  vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
-    callback = function()
-      local servers = vim
-        -- NOTE: THe file name of the LSP configuration can differ from the name
-        --       of the server.
-        .iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
-        :map(function(path)
-          return vim.fs.basename(path):match("^(.*)%.lua$")
-        end)
-        :totable()
-
-      vim.lsp.enable(servers)
-    end,
-    group = lsp_setup_group,
-    once = true,
-  })
-
-  -- LSP
-  vim.keymap.set(
-    "n",
-    "<Leader>lh",
-    vim.lsp.buf.hover,
-    { desc = "LSP buffer hover" }
-  )
-  vim.keymap.set(
-    "n",
-    "gd",
-    vim.lsp.buf.definition,
-    { desc = "LSP goto definition" }
-  )
-  vim.keymap.set(
-    "n",
-    "<Leader>la",
-    vim.lsp.buf.code_action,
-    { desc = "LSP code action" }
-  )
-  vim.keymap.set(
-    "n",
-    "<Leader>lf",
-    vim.lsp.buf.format,
-    { desc = "LSP format buffer" }
-  )
+  -- LSP keymaps.
+  vim.keymap.set("n", "<Leader>lf", function()
+    vim.lsp.buf.format({ timeout_ms = 5000 })
+  end, { desc = "LSP format buffer" })
 end
 
 return M
