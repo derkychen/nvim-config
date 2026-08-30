c44ef9f16ea51c1f5a88d5b7501b31d3a28ab12f
Derek Chen <derekjchen@proton.me>
2026-08-30 16:56:19 -0400

    fix: remove requiring modules

--- init.lua
+++ init.lua
@@ -1,15 +1,4 @@
--- Configuration entry point.
-
--- Optimize startup (experimental feature).
+---Configuration entry point.
+---
+---Optimize startup with the experimental `vim.loader`.
 vim.loader.enable()
-
--- This setup function must before the others.
-require("options").setup()
-
--- The order of the following setup functions does not matter.
-require("diagnostics").setup()
-require("help").setup()
-require("lsp").setup()
-require("sessions").setup()
-require("tabs").setup()
-require("ui2").setup()
