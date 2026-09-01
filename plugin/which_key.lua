37bbf6886f40a8af8e73100fbdadf916f30d237e
Derek Chen <derekjchen@proton.me>
2026-09-01 17:20:37 -0400

    feat: minor ui tweaks

--- plugin/which_key.lua
+++ plugin/which_key.lua
@@ -1,4 +1,15 @@
 ---Configuration for the WhichKey plugin.
-vim.pack.add({ "https://github.com/folke/which-key.nvim" })
+vim.pack.add({ 'https://github.com/folke/which-key.nvim' })
 
-require("which-key").setup({ preset = "helix" })
+require('which-key').setup({
+  -- Use the Helix-style keymap window.
+  preset = 'helix',
+
+  win = {
+    -- Make the floating window border match `vim.o.winborder`
+    border = vim.o.winborder,
+
+    -- Remove extra padding on the right.
+    col = math.huge,
+  },
+})
