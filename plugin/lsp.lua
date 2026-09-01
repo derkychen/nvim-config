0737c2f2f89fe9bd2355f7b7581641677cd99ba9
Derek Chen <derekjchen@proton.me>
2026-09-01 17:17:20 -0400

    style: ordering, naming

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -3,38 +3,37 @@
 ---Adjusts semantic highlighting to accommodate for Tree-sitter highlighting and
 ---sets up LSP servers lazily.
 
-local semantic_hls_group =
-  vim.api.nvim_create_augroup("LSPSemanticHighlights", { clear = true })
-
----Clear semantic highlights for comments in Lua.
----
----This is an exception, as it seems that Tree-sitter parsing is overridden by 
----LSP when it should not be (e.g. for `TODO` or `NOTE` comments).
-local function set_semantic_hls()
-  vim.api.nvim_set_hl(0, "@lsp.type.comment.lua", {})
-end
-
--- Set semantic highlights.
-set_semantic_hls()
-
 -- Set up all LSP servers.
 --
--- NOTE: THe file name of the LSP configuration can differ from the name
---       of the server.
+-- The file name of the LSP configuration can differ from the name of the
+-- server. However, clients will be listed by the name of their configuration.
 vim.lsp.enable(vim
-  .iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
+  .iter(vim.api.nvim_get_runtime_file('lsp/*.lua', true))
   :map(function(path)
-    return vim.fs.basename(path):match("^(.*)%.lua$")
+    return vim.fs.basename(path):match('^(.*)%.lua$')
   end)
   :totable())
 
+---Clear semantic highlights for comments in Lua.
+---
+---This is an exception, as it seems that Tree-sitter parsing is overridden by
+---LSP when it should not be (e.g. for `TODO` or `NOTE` comments).
+local function set_lua_semantic_hls()
+  vim.api.nvim_set_hl(0, '@lsp.type.comment.lua', {})
+end
+
+local semantic_hls_group =
+  vim.api.nvim_create_augroup('LSPSemanticHighlights', { clear = true })
+
 -- Update semantic highlights on a colour scheme change.
-vim.api.nvim_create_autocmd("ColorScheme", {
-  callback = set_semantic_hls,
+set_lua_semantic_hls()
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = set_lua_semantic_hls,
   group = semantic_hls_group,
 })
 
 -- Keymaps.
-vim.keymap.set("n", "<Leader>lf", function()
+vim.keymap.set('n', '<Leader>lf', function()
   vim.lsp.buf.format({ timeout_ms = 5000 })
-end, { desc = "LSP format buffer" })
+end, { desc = 'LSP format buffer' })
